c504a0ee3c523d339e91e7b0961aa54c6d0e339b
asilva-cell <achapellin@gmail.com>
2021-02-21 15:08:45 -0700

    implemented addIngredient eventEmitter from shoppingList service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,8 +3,8 @@ import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
-  ingredientAdded = new EventEmitter<Ingredient>();
-  
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 3),
     new Ingredient('Tomatoes', 5),
@@ -14,5 +14,10 @@ export class ShoppingListService {
   getIngredients() {
     //   returns a new ingredients array
     return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice())
   }
 }
